b932b665540c1a241a146b0c3e8662f3b5b3adb3
Przemyslaw <codefactorypdabrowski@gmail.com>
2020-06-08 20:57:49 +0200

    Thread safe providing objects in lazy manner.

--- app/src/main/java/com/codefactorypdabrowski/puredi/dataone/DataOneModule.kt
+++ app/src/main/java/com/codefactorypdabrowski/puredi/dataone/DataOneModule.kt
@@ -1,8 +1,10 @@
 package com.codefactorypdabrowski.puredi.dataone
 
 class DataOneModule {
-    private val dataSourceOne = DataSourceOne()
-    private val dataSourceTwo = DataSourceTwo()
 
-    val repositoryOne = RepositoryOne(dataSourceOne, dataSourceTwo)
+    val repositoryOne: RepositoryOne by lazy {
+        val dataSourceOne = DataSourceOne()
+        val dataSourceTwo = DataSourceTwo()
+        RepositoryOne(dataSourceOne, dataSourceTwo)
+    }// In Java use DoubleCheck -> https://github.com/google/dagger/blob/master/java/dagger/internal/DoubleCheck.java
 }
